b51642673695007321221866bce1b896f9d10a83
Yu-Szmiya <53136890+Yu-Szmiya@users.noreply.github.com>
2020-07-06 18:04:41 +0900

    %gに感動してるなう

---
+++ pro10601.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int month, day ;
+
+    printf("今日の日付(月）：");
+    scanf("%d",&month); 
+    printf("今日の日付(日）：");
+    scanf("%d",&day);
+
+    printf("今日は「%d月%d日」ですね\n",month,day);
+
+    return 0;
+}
