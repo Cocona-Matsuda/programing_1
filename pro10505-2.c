36fd8077dc8e438787b62fad79c562d52d1174ba
Yu-Szmiya <53136890+Yu-Szmiya@users.noreply.github.com>
2020-07-13 21:39:36 +0900

    Add files via upload

---
+++ pro10505-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int x,y;
+
+    printf("®”‚Ì’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢:");
+    scanf("%d%d",&x,&y);
+
+    printf("’·•ûŒ`‚Ì–ÊÏ‚Í%d‚Å‚·\n",x*y);
+
+    return 0;
+}
