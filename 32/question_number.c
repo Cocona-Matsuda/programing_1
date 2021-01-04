f6ad200e47326f57b74c253076f121376e17cd33
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-01-04 18:13:08 +0900

    tekitto

--- 32/question_number.c
+++ 32/question_number.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
     int max_number;
     int min_number;
@@ -11,14 +11,24 @@ int main (void)
 
     max_number = 999;
     min_number = 1;
-
-    while (max_number != min_number)
-    {
+    while (max_number != min_number){
         question_number = (max_number + min_number) / 2;
-        printf("\n貴方が思い浮かべている数は%dより大きいですか？\n",question_number);
+        printf("\n貴方が思い浮かべている数は%dより大きいですか？\n", question_number);
         printf("「はい」なら０を、「いいえ」なら１を入力してください：");
+        scanf("%d",&answer);
+
+        while (answer != 0 && answer != 1){
+            printf("0か1で答えてください＝");
+            scanf("%d",&answer);
+        }
+
+        if (answer == 0){
+            min_number = question_number + 1;
+        }else{
+            max_number = question_number;
+        }
     }
-    
+    printf("\n貴方が思い浮かべた数は%dですね\n",max_number);
 
     return 0;
 }
